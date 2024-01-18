eb5222d5e8de2e9e9d7a1df378a30ad9e2bf19c3
MRUselessPotato <svendejong10.sdj@gmail.com>
2024-01-18 16:55:52 +0100

    base site codebase

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codebase Opdrachten</title>
+    <link rel="stylesheet" href="./styles.css?v<?php echo date('l jS \of F Y h:i:s A'); ?>">
+</head>
+
+<ul>
+    <li><a href="./oppervlakte/index.php">Oppervlakte berekenen</a></li>
+</ul>
+
+</html>
