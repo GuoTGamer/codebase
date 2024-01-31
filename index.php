6a72cfec43b64e24b5d2c31922154328facc9853
MRUselessPotato <97108437@st.deltion.nl>
2024-01-31 14:20:16 +0000

    index
    - codebase homepage style toegevoegd

--- index.php
+++ index.php
@@ -6,9 +6,25 @@
     <title>Codebase Opdrachten</title>
     <link rel="stylesheet" href="./styles.css?v<?php echo date('l jS \of F Y h:i:s A'); ?>">
 </head>
+<header>
+    <h1>Codebase Opdrachten</h1>
+</header>
+<ul class="codebase">
+    <li>
+        <a class="title" href="./oppervlakte/index.php">Oppervlakte & Omtrek berekenen</a>
+        <br><br>
+        <a>Oppervlakte berekening:</a>
+        <ul>
+            <li>Vierhoek</li>
+            <li>Cirkel</li>
+            <li>Driehoek</li>
+        </ul>
+        <a>Omtrek berekening:</a>
+        <ul>
+            <li>Vierhoek</li>
+            <li>Driehoek</li>
 
-<ul>
-    <li><a href="./oppervlakte/index.php">Oppervlakte berekenen</a></li>
+    </li>
 </ul>
 
 </html>
